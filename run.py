6fd16f1112cd273ee54b7c641dccbd734dbb44eb
Karimul Makhtidi <karimul.makhtidi@navcore.com>
2023-03-02 15:05:01 +0700

    change steps

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 #     --cyclical --S_noise=1 --steps=18 --sigma_max=80", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-ffhq-64x64-uncond-ve.pkl \
-    ", shell=True, check=True, stdout=f)
+    --steps=40", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/ffhq-64x64.npz", shell=True, check=True, stdout=f)
 
@@ -17,7 +17,7 @@ subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 #     --cyclical --S_noise=1 --steps=18 --sigma_max=80", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=50000-99999 --subdirs \
     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-ffhq-64x64-uncond-ve.pkl \
-    ", shell=True, check=True, stdout=f)
+    --steps=40", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/ffhq-64x64.npz", shell=True, check=True, stdout=f)
 
@@ -27,6 +27,6 @@ subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 #     --cyclical --S_noise=1 --steps=18 --sigma_max=80", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-ffhq-64x64-uncond-ve.pkl \
-    ", shell=True, check=True, stdout=f)
+    --steps=40", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/ffhq-64x64.npz", shell=True, check=True, stdout=f)
