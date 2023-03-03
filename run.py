6c86283d48713868887e8779ba4920f29e5f6beb
Karimul Makhtidi <karimul.makhtidi@navcore.com>
2023-03-04 06:01:54 +0700

    run imagenet

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 #     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl \
 #     --cyclical --S_noise=1 --steps=18 --sigma_max=80", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
-    --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-afhqv2-64x64-uncond-vp.pkl \
-    --steps=40", shell=True, check=True, stdout=f)
+    --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-imagenet-64x64-cond-adm.pkl \
+    --steps=256", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/afhqv2-64x64.npz", shell=True, check=True, stdout=f)
 
@@ -16,8 +16,8 @@ subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 #     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl \
 #     --cyclical --S_noise=1 --steps=18 --sigma_max=80", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=50000-99999 --subdirs \
-    --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-afhqv2-64x64-uncond-vp.pkl \
-    --steps=40", shell=True, check=True, stdout=f)
+    --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-imagenet-64x64-cond-adm.pkl \
+    --steps=256", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/afhqv2-64x64.npz", shell=True, check=True, stdout=f)
 
@@ -26,7 +26,7 @@ subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 #     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl \
 #     --cyclical --S_noise=1 --steps=18 --sigma_max=80", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
-    --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-afhqv2-64x64-uncond-vp.pkl \
-    --steps=40", shell=True, check=True, stdout=f)
+    --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-imagenet-64x64-cond-adm.pkl \
+    --steps=256", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/afhqv2-64x64.npz", shell=True, check=True, stdout=f)
