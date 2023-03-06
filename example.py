3e619f405cd8724bf79ca7669a6e8f566c1fabcb
Karimul Makhtidi <karimul.makhtidi@navcore.com>
2023-03-06 16:35:58 +0700

    fix example script

--- example.py
+++ example.py
@@ -53,9 +53,6 @@ def generate_image_grid(
     t_steps = (sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * (sigma_min ** (1 / rho) - sigma_max ** (1 / rho))) ** rho
     t_steps = torch.cat([net.round_sigma(t_steps), torch.zeros_like(t_steps[:1])]) # t_N = 0
 
-    if cyclical is True:
-        print(f'Cyclical step size activate...')
-
     # Main sampling loop.
     x_next = latents.to(torch.float64) * t_steps[0]
     for i, (t_cur, t_next) in tqdm.tqdm(list(enumerate(zip(t_steps[:-1], t_steps[1:]))), unit='step'): # 0, ..., N-1
@@ -63,12 +60,16 @@ def generate_image_grid(
 
         if cyclical is True:
             # Adaptive step size of noise with cyclical
-            S_noise = 1 + adjust_learning_rate(i, total_epoch=10, lr0=S_noise - 1)
+            S_noise_new = adjust_learning_rate(i, total_epoch=num_steps, lr0=S_noise)
+        else:
+            S_noise_new = S_noise
 
         # Increase noise temporarily.
         gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
         t_hat = net.round_sigma(t_cur + gamma * t_cur)
-        x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * torch.randn_like(x_cur)
+        x_hat = x_cur + S_noise_new * S_noise * torch.randn_like(x_cur)
+        # x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * torch.randn_like(x_cur)
+
 
         # Euler step.
         denoised = net(x_hat, t_hat, class_labels).to(torch.float64)
