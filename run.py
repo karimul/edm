2cf279025731ff4d70a50d9b0df157b4c90d7d5a
Karimul Makhtidi <karimul.makhtidi@navcore.com>
2023-08-02 14:05:56 +0700

    add cifar10 run cond and uncond

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import subprocess
 
 f = open("output.txt", "w")
 
-# CIFAR10
+# CIFAR10 Conditional VE
 subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
 subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-ve.pkl \
@@ -23,6 +23,73 @@ subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fi
     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
 subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# # CIFAR10 Conditional VP
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=50000-99999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-cond-vp.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# # CIFAR10 UnConditional VE
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-ve.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=50000-99999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-ve.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-ve.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+
+# # CIFAR10 UnConditional VP
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=0-49999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-vp.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=50000-99999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-vp.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
+
+# subprocess.run("rm -rf fid-tmp/", shell=True, check=True)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 generate.py --outdir=fid-tmp --seeds=100000-149999 --subdirs \
+#     --network=https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-cifar10-32x32-uncond-vp.pkl \
+#     --cyclical --S_churn=0.0001 --S_noise=0.0001 --steps=18 --sigma_max=50", shell=True, check=True, stdout=f)
+# subprocess.run("torchrun --standalone --nproc_per_node=1 fid.py calc --images=fid-tmp \
+#     --ref=https://nvlabs-fi-cdn.nvidia.com/edm/fid-refs/cifar10-32x32.npz", shell=True, check=True, stdout=f)
 
 
 # # AFHQ V2
